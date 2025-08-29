6b19a1668845f5aa543a62571e040f59cfe051a7
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-08-30 02:44:07 +0800

    feat: navbar

---
+++ App/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <>
+        <div className="w-full flex items-center justify-center px-6 py-8 rounded-b-full shadow-md">
+          <div className="flex items-center justify-between w-[80rem]">
+            <h1 className="text-4xl font-bold text-pink-600 ">Pomoberry</h1>
+            
+            <button className="border-black bg-pink-400 px-3 py-3 rounded-lg text-white shadow-lg hover:cursor-pointer hover:shadow-pink-600 hover:transition-shadow">To Do list</button>
+            
+          </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
