ddcb4c7fc25a86036433e5eedc19876d0b2b26c4
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-09-05 12:13:08 +0800

    chore: mongoDB connection

--- Server/src/App.js
+++ Server/src/App.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const PORT = 3000;
 
+require("dotenv").config();
+
 app.listen(PORT, () => {
-    console.log('Listening to port 3000');
+  console.log("Listening to port 3000");
 });
 
-app.get('/', (req, res) => {
-    res.send('Backend is running');
-})
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
