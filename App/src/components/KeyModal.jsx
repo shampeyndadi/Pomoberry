ca746f493db59fa000e749e8347e4b3079952b28
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-09-05 14:49:42 +0800

    feat: pomokey generation added

--- App/src/components/KeyModal.jsx
+++ App/src/components/KeyModal.jsx
@@ -123,6 +123,40 @@ const StyledWrapper = styled.div`
 
 function KeyModal({ setShowKeyModal }) {
   const [checked, setChecked] = useState("enter");
+  const [pomokey, setPomokey] = useState("");
+
+  useEffect(() => {
+    const storedKey = localStorage.getItem("pomoKey");
+    if (storedKey) {
+      setPomokey(storedKey);
+    } else {
+      generatePomokey();
+    }
+  }, []);
+
+  function generatePomokey() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+
+    let key = "";
+
+    for (let i = 0; i < 3; i++) {
+      key += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+
+    for (let i = 0; i < 3; i++) {
+      key += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+
+    setPomokey(key);
+    localStorage.setItem("pomoKey", key);
+  }
+
+  useEffect(() => {
+    if (checked === "generate" && !pomokey) {
+      generatePomokey();
+    }
+  }, [checked]);
 
   function displayView() {
     if (checked === "enter") {
@@ -200,21 +234,49 @@ function KeyModal({ setShowKeyModal }) {
           </div>
 
           <div className="flex items-center space-x-3">
-            <h1 className="font-bold text-3xl">NJK432</h1>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-8 h-8 hover:cursor-pointer hover:text-pink-600"
+            <div
+              onClick={() => {
+                generatePomokey();
+              }}
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-8 h-8 hover:cursor-pointer hover:text-pink-600"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+            </div>
+
+            <h1 className="font-bold text-3xl">{pomokey}</h1>
+            <div
+              onClick={() => {
+                pomokey && navigator.clipboard.writeText(pomokey);
+                alert(`PomoKey ${pomokey} copied to clipboard!`);
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-8 h-8 hover:cursor-pointer hover:text-pink-600"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       );
