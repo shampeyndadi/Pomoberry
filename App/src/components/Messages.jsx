00b3a83f6eb965fb65c6524f19a58199057195c7
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-09-15 21:34:38 +0800

    feat: frontend displays notes

--- App/src/components/Messages.jsx
+++ App/src/components/Messages.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dog from "./Dog";
 import CreateNote from "../Services/CreateNote";
 
 function Messages({ setShowMessagesModal, account }) {
   const [message, setMessage] = useState("");
+  const [notes, setNotes] = useState(account?.notes ?? []);
   const MAX_CHARS = 30;
 
   const handleChange = (e) => {
@@ -15,23 +16,71 @@ function Messages({ setShowMessagesModal, account }) {
     }
   };
 
+  useEffect(() => {
+    setNotes(account?.notes ?? []);
+  }, [account]);
+
   const charCount = message.length;
 
   const uploadNote = async (message) => {
     try {
       const response = await CreateNote.createNote(account.pomokey, message);
-      console.log(response);
+
+      if (response.data.notes) {
+        setNotes(response.data.notes);
+      } else {
+        setNotes((prev) => [...prev, response.data]);
+      }
+
+      setMessage("");
     } catch (error) {
       console.log(error);
     }
   };
+
+  function display() {
+    if (notes.length === 0) {
+      return (
+        <div className="flex items-center justify-center w-full py-4">
+          <Dog />
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex-1 mt-4 px-5 overflow-y-auto space-y-3 border-t pt-3 h-[27rem]">
+          {notes.map((note) => (
+            <div
+              key={note._id}
+              className="w-full border rounded-lg py-2 px-2 bg-pink-300 text-white font-bold flex justify-between items-center"
+            >
+              <span>{note.content}</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 hover:cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
 
   return (
     <div
       className="fixed top-0 left-0 right-0 bottom-0 w-full h-full z-[9999] bg-red-500/20 flex items-center justify-center"
       style={{ margin: 0, padding: 0 }}
     >
-      <div className="bg-white w-[43rem] py-3">
+      <div className="bg-white w-[43rem] h-[43rem] overflow-auto py-3">
         <div className="flex flex-col">
           <div className="flex justify-between items-center px-3">
             <div></div>
@@ -81,11 +130,8 @@ function Messages({ setShowMessagesModal, account }) {
               </button>
             </div>
           </div>
-
-          <div className="flex items-center justify-center w-full py-4">
-            <Dog />
-          </div>
         </div>
+        {display()}
       </div>
     </div>
   );
