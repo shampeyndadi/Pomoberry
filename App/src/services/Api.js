bf72ad94a411bc0e36e6f61f4b8da86375a0d2c3
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-09-21 13:04:40 +0800

    fix: add dynamic API baseURL for dev/prod

--- App/src/services/Api.js
+++ App/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
     withCredentials: true,
   });
 };
